d31f5d9307d48e8d9d056f13b30f8332eb6a4166
amin mohamed <aminelcaptain20@gmail.com>
2019-11-30 17:23:25 +0200

    first commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,12 @@
+
+ 
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">ADMAN</div>
+            </div>
+        </div>
+    </div>
+</div>
